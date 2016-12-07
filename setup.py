cba10b2f1c0c3475a9d28697e6235f25fac6edc7
Shikhar Sharma <shikhar38@gmail.com>
2016-12-06 23:10:35 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='Tesseract',
+      version='0.9.0',
+      description='',
+      author='Shikhar Sharma',
+      author_email='shikhar38@gmail.com',
+      url='https://github.com/kracwarlock/tesseract',
+      download_url='https://github.com/kracwarlock/tesseract/tarball/0.9.0',
+      license='MIT',
+      install_requires=['keras'],
+      packages=find_packages())
